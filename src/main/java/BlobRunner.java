e9cb8088611055aad2ed3f9666e4e2de613e17cb
Anna Pindyurina <annasq.berg@gmail.com>
2021-09-29 18:41:30 +0300

    1

--- src/main/java/BlobRunner.java
+++ src/main/java/BlobRunner.java
@@ -9,7 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class BlobRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, IOException {
         //blob -  binary large object (значит что можно в него засунуть все что угодно ч
         // то можно представить в виде БАЙТ, обычно используется для таких обьектов как картинки видео аудио
         // в postgresql он представлен в виде bytea (byte array)
@@ -17,9 +17,36 @@ public class BlobRunner {
         //clob - character large object - можно в него запихнуть что угодно что можно
         // представить в виде символов
         // в postgresql он представлен в виде TEXT
+
+        saveImage();
+    }
+
+    private static void getImage(){
+        var sql = """
+                UPDATE aircraft
+                SET image = ?
+                WHERE id = 1
+                """;
     }
 
     private static void saveImage() throws SQLException, IOException {
+
+        var sql = """
+                UPDATE aircraft
+                SET image = ?
+                WHERE id = 1
+                """;
+
+        try (var connection = ConnectionManager.open();
+             var preparedStatement = connection.prepareStatement(sql)) {
+
+
+            preparedStatement.setBytes(1, Files.readAllBytes(Path.of("resources", "Boeing_777.jpg")));
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    /*private static void saveImage() throws SQLException, IOException {
 
         var sql = """
                 UPDATE aircraft
@@ -38,5 +65,5 @@ public class BlobRunner {
             preparedStatement.executeUpdate();
             connection.commit();
         }
-    }
+    }*/
 }
